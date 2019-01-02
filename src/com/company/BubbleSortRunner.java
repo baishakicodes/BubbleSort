24746ffe89977bc85cf38f1a6c1663adc9a859b8
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2019-01-02 17:18:12 -0500

    added in the bubbleSort method. added some comments to check my understanding.

--- src/com/company/BubbleSortRunner.java
+++ src/com/company/BubbleSortRunner.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+
 public class BubbleSortRunner {
 
     public static void main(String[] args) {
 	// write your code here
+        int[] testArr= {8,6,7,5,3,0,9,10,1,2,4};
+
+        //print out the unsorted array
+        System.out.print("Before: ");
+        for(int num:testArr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
+        //sorting takes place here
+        bubbleSort.bubbleSort(testArr);
+
+        //Print out new sorted array
+        System.out.print("After: ");
+        for(int num:testArr){
+            System.out.print(num+" ");
+        }
+        System.out.println();
     }
 }
--- src/com/company/bubbleSort.java
+++ src/com/company/bubbleSort.java
@@ -2,10 +2,37 @@ package com.company;
 
 public class bubbleSort {
     public static void bubbleSort(int[] arr){
-        boolean
-        while(arr[c-2]> arr[c-1]){
+        boolean swap = true;
+        while(swap){
+            //change the variable swap to false because nothing has been swapped yet
+            //also we want to continue swapping until the whole array is ordered so we don't want the variable to be true prematurely
+            swap = false;
+            for(int i =0; i<arr.length-1; i++){
+                if(arr[i]>arr[i+1]){
+                    int temp = arr[i];
+                    arr[i]=arr[i+1];
+                    arr[i+1]=temp;
+                    //swapping has taken place so swap is true
+                    swap = true;
+                }
+            }
 
         }
+
+        //*another method for bubble sort
+
+        //*the first for loop is for the out iteration
+        //for(int i =0; i<arr.length; i++){
+            //*the second for loop is for the actual swapping in the first outer iteration
+            //*the precondition j<arr.length-1 is so that we don't keep on going to the end of the array that is already sorted
+            //for(int j=1; j<arr.length-i; j++){
+                //if(arr[j-1]>arr[j]) {
+                    //int temp = arr[j - 1];
+                    //arr[j - 1] = arr[j];
+                    //arr[j] = temp;
+                //}
+            //}
+        //}
 
     }
 
