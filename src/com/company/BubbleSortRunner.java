ce622989822a1a79097f80f6a6035a9fdeec4417
BT_1N3_31 <baishakicodes>
2019-01-08 08:40:48 -0500

    need to fix bubblesort for strings

--- src/com/company/BubbleSortRunner.java
+++ src/com/company/BubbleSortRunner.java
@@ -8,28 +8,29 @@ public class BubbleSortRunner {
         int[] testArr= {3,1,8,10,4,6,5,2,0,7,9};
         int[] copyOfTestArr = {3,1,8,10,4,6,5,2,0,7,9};
         int[] randArr = sortingUtil.randIntArr(6);
+        String[] arrOfStrings={"array","is","a","test","this"};
 
         //print out the unsorted array
         System.out.print("Before: ");
-        for(int num: randArr){
+        for(String num: arrOfStrings){
             System.out.print(num+" ");
         }
         System.out.println();
         //sorting takes place here
-        bubbleSort.selectionSort(randArr);
+        bubbleSort.bubbleSort(arrOfStrings);
 
         //Print out new sorted array
         System.out.print("After: ");
-        for(int num:randArr){
+        for(String num:arrOfStrings){
             System.out.print(num+" ");
         }
         System.out.println();
-        System.out.println("Is the array sorted?" +sortingUtil.isSorted(randArr));
-        System.out.println("CheckSum: "+sortingUtil.checkSum(copyOfTestArr, testArr));
+        //System.out.println("Is the array sorted?" +sortingUtil.isSorted(randArr));
+        //System.out.println("CheckSum: "+sortingUtil.checkSum(copyOfTestArr, testArr));
        // System.out.println(sortingUtil.checkSum(randArr));
 
-        long time = System.nanoTime();
-        bubbleSort.bubbleSort(randArr);
-        time = System.nanoTime()-time;
+        //long time = System.nanoTime();
+        //bubbleSort.bubbleSort(randArr);
+        //time = System.nanoTime()-time;
     }
 }
