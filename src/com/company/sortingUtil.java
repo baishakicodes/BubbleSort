d94bb9dd103ced585fe92451bc7e9c0c30757fc1
baishakicodes <43207472+baishakicodes@users.noreply.github.com>
2019-01-17 21:49:32 -0500

    added in binary search methods

--- src/com/company/sortingUtil.java
+++ src/com/company/sortingUtil.java
@@ -122,5 +122,45 @@ public class sortingUtil {
         }
         return arr;
     }
+    //two different methods for binary search
+
+    //this method uses a for loop
+    public static int binarySearch(int[] elements, int target) {
+        inPlaceSorts.bubbleSortII(elements);
+        int max =elements.length-1;
+        int min = 0;
+        for (int i = (min+max)/2; min<=max;i=(min+max)/2 ) {
+            if (elements[i] == target) {
+                return i;
+            } else if (elements[i] > target) {
+                max = i - 1;
+            } else if (elements[i] < target) {
+                min = i + 1;
+            }
+        }
+        return -1;
+    }
+    //this method uses a while loop
+    public static int binarySearch2(int[] elements, int target){
+        inPlaceSorts.bubbleSortII(elements);
+        int max =elements.length-1;
+        int min = 0;
+        int guess = (min+max)/2;
+        boolean checker = true;
+        while(min<=max){
+            if(elements[guess]==target){
+                return guess;
+            }
+            else if(elements[guess]>target){
+                max = guess-1;
+                guess = (min+max)/2;
+            }
+            else if(elements[guess]<target){
+                min=guess+1;
+                guess=(min+max)/2;
+            }
+        }
+        return -1;
+    }
 
 }
